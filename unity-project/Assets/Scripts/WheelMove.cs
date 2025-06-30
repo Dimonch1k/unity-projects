02c1739d96cac59c2eb4647ed6d987d45f794138
Dimonch1k <dmytroleskiv77@gmail.com>
2025-06-30 23:00:14 +0300

    Finished 2nd HW from Unity

---
+++ unity-project/Assets/Scripts/WheelMove.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class WheelMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Surface"))
+        {
+            isGrounded = true;
+        }
+    }
+}
